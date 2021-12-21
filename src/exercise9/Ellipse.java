cd4b22a2aa05952e50ff4ea099a751c7ffc6c3ab
zlatinas <>
2021-12-21 11:31:29 +0000

    z

--- src/exercise9/Ellipse.java
+++ src/exercise9/Ellipse.java
@@ -2,69 +2,69 @@ package exercise9;
 
 public class Ellipse {
   private Point startPoint;
-  private double x;
-  private double y;
+    private double a;
+    private double b;
 
-  public Ellipse(){
-    startPoint = new Point(0, 0);
-    x = 1;
-    y = 1;
-  }
+    public Ellipse() {
+        startPoint = new Point(0, 0);
+        a = 1;
+        b = 1;
+    }
 
-  public Ellipse(Point startPoint, double a, double b){
-    this.startPoint = new Point(startPoint);
-    this.x = x;
-    this.y = y;
-  }
-public Ellipse(Ellipse otherEllipse) {
+    public Ellipse(Point startPoint, double a, double b) {
+        this.startPoint = new Point(startPoint);
+        this.a = a;
+        this.b = b;
+    }
+
+    public Ellipse(Ellipse otherEllipse) {
         startPoint = new Point(otherEllipse.startPoint);
-        x = otherEllipse.x;
-        y = otherEllipse.y;
-}
+        a = otherEllipse.a;
+        b = otherEllipse.b;
+    }
 
-public boolean isValid() {
-  return x > 0 && y > 0;
-}
+    public boolean isValid() {
+        return a > 0 && b > 0;
+    }
 
-public void initialize(){
-  do {
+    public void initialize() {
+        do {
             System.out.println("Start point: ");
             startPoint.initialize();
             System.out.print("Enter a: ");
-            x = Utils.INPUT.nextDouble();
+            a = Utils.INPUT.nextDouble();
             System.out.print("Enter b: ");
-            y = Utils.INPUT.nextDouble();
+            b = Utils.INPUT.nextDouble();
         } while (!isValid());
-}
-
-public double calculatePerimeter(){
-  return Math.PI * (3 * (x + y) - Math.sqrt((3 * x + y)* (x + 3 * y)));
-}
-
-public double calculateArea(){
-  return Math.PI * x * y;
-}
-
- public String getType() {
-        return (x == y) ? "Circle" : "Ellipse";
- }
-
- public String toString() {
-        return String.format("%s-[%s, %s], %s, P=%s, A=%s", startPoint, x, y, getType(), calculatePerimeter(), calculateArea());
     }
 
-  
- public boolean equal(Ellipse otherEllipse) {
-        boolean sameX = Utils.equals(x, otherEllipse.x);
-        boolean sameY = Utils.equals(y, otherEllipse.y);
-        boolean sameXReversed = Utils.equals(x, otherEllipse.y);
-        boolean sameYReversed = Utils.equals(y, otherEllipse.x);
-
-        return (sameX && sameY) || (sameXReversed && sameYReversed);
+    public double calculatePerimeter() {
+        return Math.PI*(3*(a + b) - Math.sqrt((3*a+b)*(a+3*b)));
     }
 
+    public double calculateArea() {
+        return a * b * Math.PI;
+    }
 
+    public String getType() {
+      if(a==b) {
+        return "Circle";
+      }
+      else {
+        return "Ellipse";
+      }
+    }
 
-  
+    public String toString() {
+        return String.format("%s-[%s, %s], %s, P=%s, A=%s", startPoint, a, b, getType(), calculatePerimeter(), calculateArea());
+    }
 
+    public boolean equal(Ellipse otherEllipse) {
+        boolean sameA = Utils.equals(a, otherEllipse.a);
+        boolean sameB = Utils.equals(b, otherEllipse.b);
+        boolean sameAReversed = Utils.equals(a, otherEllipse.a);
+        boolean sameBReversed = Utils.equals(b, otherEllipse.b);
+
+        return (sameA && sameB) || (sameAReversed && sameBReversed);
+    }
 }
