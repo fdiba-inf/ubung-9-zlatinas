1f3e6733bd075a8fbdef2d638997c1d792e931e8
zlatinas <>
2021-12-21 11:29:02 +0000

    z

--- src/exercise9/Ellipse.java
+++ src/exercise9/Ellipse.java
@@ -1,4 +1,70 @@
 package exercise9;
 
 public class Ellipse {
+  private Point startPoint;
+  private double x;
+  private double y;
+
+  public Ellipse(){
+    startPoint = new Point(0, 0);
+    x = 1;
+    y = 1;
+  }
+
+  public Ellipse(Point startPoint, double a, double b){
+    this.startPoint = new Point(startPoint);
+    this.x = x;
+    this.y = y;
+  }
+public Ellipse(Ellipse otherEllipse) {
+        startPoint = new Point(otherEllipse.startPoint);
+        x = otherEllipse.x;
+        y = otherEllipse.y;
+}
+
+public boolean isValid() {
+  return x > 0 && y > 0;
+}
+
+public void initialize(){
+  do {
+            System.out.println("Start point: ");
+            startPoint.initialize();
+            System.out.print("Enter a: ");
+            x = Utils.INPUT.nextDouble();
+            System.out.print("Enter b: ");
+            y = Utils.INPUT.nextDouble();
+        } while (!isValid());
+}
+
+public double calculatePerimeter(){
+  return Math.PI * (3 * (x + y) - Math.sqrt((3 * x + y)* (x + 3 * y)));
+}
+
+public double calculateArea(){
+  return Math.PI * x * y;
+}
+
+ public String getType() {
+        return (x == y) ? "Circle" : "Ellipse";
+ }
+
+ public String toString() {
+        return String.format("%s-[%s, %s], %s, P=%s, A=%s", startPoint, x, y, getType(), calculatePerimeter(), calculateArea());
+    }
+
+  
+ public boolean equal(Ellipse otherEllipse) {
+        boolean sameX = Utils.equals(x, otherEllipse.x);
+        boolean sameY = Utils.equals(y, otherEllipse.y);
+        boolean sameXReversed = Utils.equals(x, otherEllipse.y);
+        boolean sameYReversed = Utils.equals(y, otherEllipse.x);
+
+        return (sameX && sameY) || (sameXReversed && sameYReversed);
+    }
+
+
+
+  
+
 }
--- src/exercise9/EllipseDemo.java
+++ src/exercise9/EllipseDemo.java
@@ -1,7 +1,21 @@
 package exercise9;
-
 public class EllipseDemo {
     public static void main(String[] args) {
+        Ellipse ellipse1 = new Ellipse();
+        Point startPoint2 = new Point(2, 3);
+        Ellipse ellipse2 = new Ellipse(startPoint2, 5, 6);
+        Ellipse ellipse3 = new Ellipse(ellipse2);
+        Ellipse ellipse4 = new Ellipse();
+       ellipse4.initialize();
 
+        System.out.println("Ellipse 1: " + ellipse1);
+        System.out.println("Ellipse 2: " + ellipse2);
+        System.out.println("Ellipse 3: " + ellipse3);
+        System.out.println("Ellipse 4: " + ellipse4);
+
+        System.out.println("Ellipse 2 equals Ellipse 1: " +  ellipse2.equal(ellipse1));
+        System.out.println("Ellipse 2 equals Ellipse 3: " +  ellipse2.equal(ellipse3));
+    }
+}
     }
 }
